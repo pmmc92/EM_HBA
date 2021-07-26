092b68a07000e7e511f026a3e45121295c02122e
pmmc92 <pmmc92@users.noreply.github.com>
2021-07-26 11:51:48 +0100

    3rd commit

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
 mkdir -p ~/.streamlit/
-
 echo "\
 [general]\n\
-email = \"email@domain\"\n\
+email = \"your@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
-
 echo "\
 [server]\n\
 headless = true\n\
